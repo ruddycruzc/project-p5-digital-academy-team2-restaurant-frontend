ca4ab4812f945f0fdf5477766d4ec3f2f226ff30
Ruddy <ruddy391@gmail.com>
2026-09-09 14:36:45 +0200

    feat: update router configuration to include HomeView component

--- src/router/index.js
+++ src/router/index.js
@@ -1,13 +1,27 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import LoginView from '../Views/LoginView.vue'
-import RegisterView from '../Views/RegisterView.vue'
+import { createRouter, createWebHistory } from "vue-router";
+import HomeView from "../Views/HomeView.vue";
+import LoginView from "../Views/LoginView.vue";
+import RegisterView from "../Views/RegisterView.vue";
 
 const routes = [
-  { path: '/', name: 'login', component: LoginView, alias: '/login' },
-  { path: '/register', name: 'register', component: RegisterView },
-]
+ {
+      path: '/',
+      name: 'home',
+      component: HomeView
+    },
+    {
+      path: '/login',
+      name: 'login',
+      component: LoginView
+    },
+    {
+      path: '/register',
+      name: 'register',
+      component: RegisterView
+    }
+];
 
 export default createRouter({
   history: createWebHistory(),
   routes,
-})
+});
